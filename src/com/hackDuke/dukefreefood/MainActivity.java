acf64a7e047d694d6169cb65bf36fc0219e71843
David Hernandez <davidamh@vt.edu>
2013-11-16 18:14:29 -0500

    Added API key for Google Maps.

--- src/com/hackDuke/dukefreefood/MainActivity.java
+++ src/com/hackDuke/dukefreefood/MainActivity.java
@@ -2,7 +2,6 @@ package com.hackDuke.dukefreefood;
 
 import java.util.Locale;
 
-import android.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
